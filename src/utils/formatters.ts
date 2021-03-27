1831a4a55321a910149a4c4eace34f01a52c2e34
Gabriel Ferreira <gabriielbs@gmail.com>
2021-03-27 20:19:27 -0300

    refactor: add getModificationsNumber function into formatters

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,2 +1,15 @@
 export const getAlertTextMessage = (gitDiffText: string) =>
   `What do you think about opening a Pull Request? 🥰\n~ You already have ${gitDiffText}`;
+
+const sumAddedWithDeleted = (acc: number, curr: number) => {
+  acc += curr;
+  return acc;
+};
+
+export const getModificationsNumber = (modifications: string) => {
+  return modifications
+    .split(",")
+    .slice(1)
+    .map((item) => Number(item.replace(/[^0-9]+/g, "")))
+    .reduce(sumAddedWithDeleted, 0);
+};
