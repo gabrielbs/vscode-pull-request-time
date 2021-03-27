240078442ca8d7ba45c09edb580d52cf77d60395
Gabriel Ferreira <gabriielbs@gmail.com>
2021-03-27 20:18:21 -0300

    refactor: rename functions

--- src/extension.test.ts
+++ src/extension.test.ts
@@ -14,13 +14,13 @@ describe("extension", () => {
     jest
       .spyOn(extension, "getModifiedFilesAmount")
       .mockReturnValue(Promise.resolve("   20"));
-    jest.spyOn(extension, "performPullRequestChecks");
+    jest.spyOn(extension, "dispatchMessage");
   });
 
   it("should work properly", async () => {
     jest.useFakeTimers();
     extension.activate();
     await jest.advanceTimersByTime(1000 * 60 * 5);
-    expect(extension.performPullRequestChecks).toHaveBeenCalled();
+    expect(extension.dispatchMessage).toHaveBeenCalled();
   });
 });
--- src/extension.ts
+++ src/extension.ts
@@ -1,59 +1,31 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from "vscode";
 import { execShellCommand } from "./utils/exec-shell-comands";
-import { getAlertTextMessage } from "./utils/formatters";
+import {
+  getAlertTextMessage,
+  getModificationsNumber,
+} from "./utils/formatters";
+import {
+  MESSAGE,
+  INTERVAL,
+  MAX_MODIFICATIONS_NUMBER,
+  schema,
+} from "./utils/options";
 
-// Receive this consts from extension config
-// const MAX_MODIFICATIONS_NUMBER = 200;
-const MAX_MODIFICATIONS_NUMBER = 1;
-
-enum ALERT_OPTIONS {
-  OPEN_IT = "Open it 💜",
-  SNOOZE_5 = "Snooze for 5 minutes",
-  SNOOZE_10 = "Snooze for 10 minutes",
-}
-
-enum INTERVALS {
-  // DEFAULT = 1000 * 5, // five seconds
-  // OPEN_IT = 1000 * 60 * 2, // two minutes
-  // FIVE_MINUTES = 1000 * 60 * 5, // five minutes
-  // TEN_MINUTES = 1000 * 60 * 10, // 10 minutes
-  DEFAULT = 1000 * 1, // five seconds
-  OPEN_IT = 1000 * 5, // two minutes
-  FIVE_MINUTES = 1000 * 10, // five minutes
-  TEN_MINUTES = 1000 * 20, // 10 minutes
-}
-
-const alertOptions = [
-  {
-    title: ALERT_OPTIONS.OPEN_IT,
-    interval: INTERVALS.OPEN_IT,
-  },
-  { title: ALERT_OPTIONS.SNOOZE_5, interval: INTERVALS.FIVE_MINUTES },
-  { title: ALERT_OPTIONS.SNOOZE_10, interval: INTERVALS.TEN_MINUTES },
-];
-
-const getTimeoutInterval = (selected?: ALERT_OPTIONS) => {
+const getTimeoutInterval = (selected?: MESSAGE) => {
   return (
-    alertOptions.find((option) => option.title === selected)?.interval ||
-    INTERVALS.DEFAULT
+    schema.find((option) => option.title === selected)?.interval ||
+    INTERVAL.default
   );
 };
 
-export const performPullRequestChecks = async (
-  filesChanged: number,
-  gitDiffText: string
-) => {
-  if (filesChanged > MAX_MODIFICATIONS_NUMBER) {
-    try {
-      const { showInformationMessage } = vscode.window;
-      const message = getAlertTextMessage(gitDiffText);
-      const selection = await showInformationMessage(message, ...alertOptions);
+export const dispatchMessage = async (message: string) => {
+  try {
+    const { showInformationMessage } = vscode.window;
+    const selection = await showInformationMessage(message, ...schema);
 
-      check(selection?.title);
-    } catch (error) {
-      console.log(error);
-    }
+    loader(selection?.title);
+  } catch (error) {
+    console.log(error);
   }
 };
 
@@ -62,35 +34,25 @@ export const getModifiedFilesAmount = async (): Promise<string> => {
   return String(cmd);
 };
 
-const sumAddedWithDeleted = (acc: number, curr: number) => {
-  acc += curr;
-  return acc;
-};
-
-const getModificationsNumber = (modifications: string) => {
-  return modifications
-    .slice(1)
-    .split(",")
-    .map((item) => Number(item.replace(/[^0-9]+/g, "")))
-    .reduce(sumAddedWithDeleted, 0);
-};
-
-const app = async () => {
+const checkFilesDiff = async () => {
   const modifications = await getModifiedFilesAmount();
   const modificationsNumber = getModificationsNumber(modifications);
-  performPullRequestChecks(modificationsNumber, modifications);
+  if (modificationsNumber > MAX_MODIFICATIONS_NUMBER) {
+    const message = getAlertTextMessage(modifications);
+    dispatchMessage(message);
+  }
 };
 
 let interval: NodeJS.Timeout;
 
-const check = (selected?: ALERT_OPTIONS) => {
+const loader = (selected?: MESSAGE) => {
   clearInterval(interval);
-  interval = setInterval(app, getTimeoutInterval(selected));
+  interval = setInterval(checkFilesDiff, getTimeoutInterval(selected));
 };
 
 export function activate() {
   console.log("Extension is running =)");
-  check();
+  loader();
 }
 
 export function deactivate() {
