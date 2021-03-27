aab05f8a1b004da5b350e9ea7a74d72eb04bc8b3
Gabriel Ferreira <gabriielbs@gmail.com>
2021-03-27 20:18:45 -0300

    fix: import path

--- src/utils/exec-shell-comands.ts
+++ src/utils/exec-shell-comands.ts
@@ -1,4 +1,4 @@
-import { exec } from "node:child_process";
+import { exec } from "child_process";
 
 export const execShellCommand = (cmd: string): Promise<string> => {
   return new Promise((resolve, reject) => {
