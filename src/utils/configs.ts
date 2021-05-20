cd265f50f85bb9092c97a564038ae9909cec6834
Gabriel Ferreira <gabriielbs@gmail.com>
2021-05-19 21:01:59 -0300

    refactor(config): remove ten minutes option

--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -3,7 +3,6 @@ export const MAX_MODIFICATIONS_NUMBER = 200;
 export enum MESSAGE {
   openIt = "Open it 💜",
   snoozeForFive = "Snooze for 5 minutes",
-  snoozeForTen = "Snooze for 10 minutes",
   snoozeCustom = "Snooze for custom minutes",
 }
 
@@ -11,7 +10,6 @@ export enum INTERVAL {
   default = 1000 * 5, // five seconds
   openIt = 1000 * 60 * 2, // two minutes
   fiveMinutes = 1000 * 60 * 5,
-  tenMinutes = 1000 * 60 * 10,
 }
 
 export type OptionSchemaType = {
@@ -25,6 +23,5 @@ export const optionsSchema: OptionSchemaType[] = [
     interval: INTERVAL.openIt,
   },
   { title: MESSAGE.snoozeForFive, interval: INTERVAL.fiveMinutes },
-  { title: MESSAGE.snoozeForTen, interval: INTERVAL.tenMinutes },
   { title: MESSAGE.snoozeCustom },
 ];
